00b6aaa5a3cd93505fa67e323856274638b2077b
joel_yoffe <joel.y.mobile@gmail.com>
2021-10-16 09:51:34 +1100

    adding folders

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+var navbar = document.querySelector('#tmNav');
+
+document.addEventListener('scroll', () => {
+    var scroll = getElementScrollScale(document.documentElement)
+    if (scroll < 0.25) {
+        navbar.classList.remove('scroll');
+    }
+    else {
+        navbar.classList.add('scroll')
+    }
+})
+
+
+function getElementScrollScale(domElement){
+    return domElement.scrollTop / (domElement.scrollHeight - domElement.clientHeight);
+}
